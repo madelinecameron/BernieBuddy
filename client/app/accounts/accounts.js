662435c13b1d10818a60344d34112bad3037821c
Madeline Cameron <madeline.a.cameron@gmail.com>
2015-10-13 18:02:23 -0500

    Sigh, missed a few files.

---
+++ client/app/accounts/accounts.js
@@ -0,0 +1,12 @@
+Template.accounts.events({
+  'click .btn-facebook': function(event, err) {
+    Meteor.loginWithFacebook({
+      requestPermissions: ['email']
+    });
+  },
+  'click .btn-twitter': function(event, err) {
+    Meteor.loginWithTwitter({
+      requestPermissions: ['email']
+    });
+  }
+})
