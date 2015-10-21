6bae92dc456c273fcb1f356ac8c3ff86240d98ad
Madeline Cameron <madeline.a.cameron@gmail.com>
2015-10-21 12:21:26 -0500

    Fix login

--- client/app/accounts/accounts.js
+++ client/app/accounts/accounts.js
@@ -2,14 +2,20 @@ Template.accounts.events({
   "click .btn-facebook": function(event, err) {
     Meteor.loginWithFacebook({
       requestPermissions: ["email"]
-    })
-    window.location.replace('/');
+    }, function(err) {
+      if(!err) {
+        window.location.replace('/');  //Redirect
+      }
+    });
   },
   "click .btn-twitter": function(event, err) {
     Meteor.loginWithTwitter({
       requestPermissions: ["email"]
+    }, function(err) {
+      if(!err) {
+        window.location.replace('/');  //Redirect
+      }
     })
-    window.location.replace('/');
   }
 })
 
